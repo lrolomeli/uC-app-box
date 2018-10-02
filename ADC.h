7128e8c2809d79fa24eabec49423918208570ec6
lrolomeli <31136136+lrolomeli@users.noreply.github.com>
2018-10-02 16:36:21 -0500

    Add files via upload

---
+++ ADC.h
@@ -0,0 +1,23 @@
+/*
+ * ADC.h
+ *
+ *  Created on: 24 oct. 2017
+ *      Author: manuel
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+void ADC_Init(void);
+
+uint8 ADC_result(void);
+
+uint32 getFarenheit(void);
+
+uint32 getCelsius(void);
+
+
+
+#endif /* ADC_H_ */
+
